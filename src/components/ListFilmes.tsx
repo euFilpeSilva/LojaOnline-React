94273881049de1fa8423b12e734bcc0996b8960e
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-07-08 16:31:50 -0300

    renderizando imagens de filmes

--- src/components/ListFilmes.tsx
+++ src/components/ListFilmes.tsx
@@ -10,6 +10,7 @@ export default function ListFilmes() {
     const [filmes, setFilmes] = useState([]);
 
     const baseURL = "https://api.themoviedb.org/3/movie/popular?api_key=c53174418b2a81eacf8a7966fa850c98&language=pt-BR"
+    const image_path = "https://image.tmdb.org/t/p/w500"
     
     useEffect(() => {
         axios.get(baseURL)
@@ -20,9 +21,10 @@ export default function ListFilmes() {
    
   return(
         <div>
-         <ul className="flex-1">
+         <ul className=" grid grid-cols-4 gap-2 mt-10 ">
         {filmes.map(filme => (
-            <li key={filme.id}>
+            <li key={filme.id} className="border-1 border-black rounded">
+                 <img src={`${image_path}${filme.poster_path}`}></img>
                  <strong>{filme.title}</strong>
             </li>
         )) }
