fc25ce8a177ef186eef4ea95731a33f3b127d184
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-07-11 21:35:51 -0300

    atualizando logica dos genres e alguns detalhes na Sidebar

--- src/components/GenreFilms.tsx
+++ src/components/GenreFilms.tsx
@@ -8,6 +8,7 @@ type GenreFilmsProps = {
 }
 
 type Genres = {
+    genreList: any;
     id: number,
     name: string
 }
@@ -29,10 +30,13 @@ export default function GenreFilms(props: GenreFilmsProps) {
             });
     }, []);
 
+    // Para cada filme, substitui o array de ids de genero por um novo array apenas com a primeira posição do antigo
+    props.film.genre_ids = [props.film.genre_ids[0]]
+
         return (
 
-            <div className="p-2 text-center leading-5 text-gray-700 bg-white">
-
+            <div className="pl-3 pt-0 pb-5 text-center leading-5 text-gray-700 bg-white flex gap-1 text-xs ">
+                    <strong className="">Gênero:</strong>
                 {
                     genresList.map(genre => (
                         props.film.genre_ids.includes(genre.id) ? <div>{genre.name}</div> : <></>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,17 +4,26 @@ type SideBarProps = {
 }
 
 export default function Sidebar(props : SideBarProps) {
-       
+      
+
+
     let styles = {
         width: props.isOpen ? "300px" : "0px",
         transition: "1000ms"
     }
-
     return (
      <>
-        <div className="border-l-[0.05vw] dark:bg-gray-800 top-15 right-0 h-full fixed border-slate-600 mt-24" style={styles}>
-            <h2 className="text-2-xl text-white p-5">Carrinho</h2>
+        <div className="border-l-[0.05vw] dark:bg-gray-800 top-15 right-0 fixed border-slate-600 mt-24" style={styles}>
+          <div className="h-[35vw]">
+            <h2 className="text-2-xl text-white p-5">Carrinho:</h2>
+          </div>
+            <p className="text-white pl-1">Total:</p>
+            <button className="bg-blue-600 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded ml-1 mt-1 transform motion-safe:hover:scale-105 ... transition-all shadow-sm" style={styles}>
+                  Finalizar compra
+            </button>
         </div>
     </>
     )
 }
+
+
